038972a4c7d7a61cb50c84ef83bb3e836ff85205
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-12 05:58:37 +0600

    correct answer added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,16 +1,41 @@
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Question = ({ questions }) => {
   const { question } = questions;
   const options = questions.options;
+  // console.log(questions)
+  const correctAnswer = () => {
+    toast.success((questions.correctAnswer), {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
   return (
     <div className="border border-info my-lg-5 my-2 p-lg-4 rounded bg-secondary d-flex flex-column align-items-center justify-content-center position-relative">
       <div className="d-flex">
         <h3 className="fw-bold text-white w-75 mx-auto">{question}</h3>
         <div className="position-absolute top-0 end-0 p-3">
-          <FontAwesomeIcon icon={faEye} className="text-info"></FontAwesomeIcon>
+          <FontAwesomeIcon onClick={correctAnswer} icon={faEye} className="text-info"></FontAwesomeIcon>
+          <ToastContainer
+         position="top-center"
+         autoClose={5000}
+         hideProgressBar={false}
+         newestOnTop={false}
+         closeOnClick
+         rtl={false}
+         pauseOnFocusLoss
+         draggable
+         pauseOnHover
+       />
         </div>
       </div>
       {options.map((option) => (
@@ -18,7 +43,9 @@ const Question = ({ questions }) => {
           <input className="mx-1" type="radio" name="" id="" />
           <span className="fw-bold mx-1">{option}</span>
         </div>
+         
       ))}
+   
     </div>
   );
 };
