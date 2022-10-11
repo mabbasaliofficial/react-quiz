8c5b367500bc5c82a8ff302a5a577e9c32d4f1a3
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-11 19:53:11 +0600

    blogs added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="border border-info m-5 p-2 rounded">
+        <h3 className="fw-bold">1. What is the purpose of react router?</h3>
+        <p>
+          React Router is a standard library for routing in React. It enables the navigation among
+          views of various components in a React Application, allows changing the browser URL, and
+          keeps the UI in sync with the URL.
+        </p>
+      </div>
+      <div className="border border-info m-5 p-2 rounded">
+        <h3 className="fw-bold">2. How does context API work?</h3>
+        <p>
+          React.createContext() is all you need. It returns a consumer and a provider. Provider is a
+          component that as it's names suggests provides the state to its children. It will hold the
+          "store" and be the parent of all the components that might need that store. Consumer as it
+          so happens is a component that consumes and uses the state. More information can be found
+          on React's documentation page.
+        </p>
+      </div>
+      <div className="border border-info m-5 p-2 rounded">
+        <h3 className="fw-bold">3. What is useRef?</h3>
+        <p>
+          The useRef Hook allows you to persist values between renders. It can be used to store a
+          mutable value that does not cause a re-render when updated. It can be used to access a DOM
+          element directly.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const Question = ({questions}) => {
-    const {question} = questions;
-    const options = questions.options;
-    return (
-        <div >
-            <div className='border border-info m-5 p-4 rounded bg-secondary d-flex flex-column align-items-center justify-content-center'>
-            <h3 className='fw-bold text-white'>{question}</h3>
-            {
-                options.map(option => <div className='border border-info d-flex align-items-center justify-content-center bg-white p-2 my-2 w-75 rounded'>
-                    <input className='mx-1' type="radio" name="" id="" />
-                    <span className='fw-bold mx-1'>{option}</span>
-                </div>)
-            }
+const Question = ({ questions }) => {
+  const { question } = questions;
+  const options = questions.options;
+  return (
+    <div className="border border-info m-5 p-4 rounded bg-secondary d-flex flex-column align-items-center justify-content-center">
+        
+      <div className="d-flex">
+     
+        <h3 className="fw-bold text-white">{question}</h3>
+        <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
         </div>
-        </div>
-    );
+        {options.map((option) => (
+          <div className="border border-info d-flex align-items-center justify-content-center bg-white p-2 my-2 w-75 rounded">
+            <input className="mx-1" type="radio" name="" id="" />
+            <span className="fw-bold mx-1">{option}</span>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Question;
