795ae2f76a85499a7962b7dc060349b0700bd82a
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-12 09:41:34 +0600

    all done

--- README.md
+++ README.md
@@ -4,14 +4,6 @@ My Website link is [Web Learning](https://quiz-competition.netlify.app/).
 
 ## About this project...
 
-**Its a learning website**
-
-**You can learn about website**
-
-**You can also learn github**
-
-### About Quiz...
-
 * React
 * CSS
 * Javascript
--- src/components/QuizContainer/QuizContainer.js
+++ src/components/QuizContainer/QuizContainer.js
@@ -6,7 +6,7 @@ const QuizContainer = ({ quiz }) => {
   const {id, name, logo, total } = quiz;
   return (
     <div className='mt-5 mb-5'>
-      <Card style={{ width: "15rem" }} className='mx-1 border border-secondary'>
+      <Card className=' mx-1 border border-secondary'>
         <Card.Img variant="top" src={logo} className='bg-secondary'/>
         <Card.Body className="bg-light">
           <Card.Title className="fw-bold">{name}</Card.Title>
