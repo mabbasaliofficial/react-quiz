9e79779aaeb74948d367b46abb7857aa3d397dfe
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-12 09:03:12 +0600

    readme.md file added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,8 @@ import logo from './quiz.png'
 
 const Header = () => {
     return (
-        <div className='d-lg-flex mt-5 justify-content-center'>
-            <img style={{height: '250px'}} className='w-25 mx-auto' src={logo} alt="" />
+        <div className='d-lg-flex p-5 justify-content-center bg-light'>
+            <img style={{height: '250px'}} className='w-lg-25 mx-auto' src={logo} alt="" />
             <div className='w-50 ms-lg-5 mx-auto'>
                 <h1 className='fw-bold'>Grow Your Knowledge</h1>
                 <p>Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network). Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services. A more comprehensive list of tasks to which Web development commonly refers, may include Web engineering, Web design, Web content development, client liaison, client-side/server-side scripting, Web server and network security configuration, and e-commerce development.</p>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,7 +6,7 @@ const Quiz = () => {
     const quiz = useLoaderData()
     const {questions} =quiz.data;
     return (
-        <div className='m-lg-5 m-2'>
+        <div className='p-lg-5 p-2 bg-light'>
             <h2 className='fw-bold'>Quiz of {quiz.data.name}</h2>
             {
                 questions.map(question => <Question
